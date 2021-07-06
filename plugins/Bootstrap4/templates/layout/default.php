32f78294cec29fe3356a66f3f7fb4c0fcb4c8712
Antonio García Cortés <agacor@gmail.com>
2021-07-07 00:33:12 +0200

    Logo Minderest

--- plugins/Bootstrap4/templates/layout/default.php
+++ plugins/Bootstrap4/templates/layout/default.php
@@ -30,11 +30,18 @@
 
     <!-- Navigation -->
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Minderest</a>
+        <?=$this->Html->link(
+            $this->Html->image('/img/minderest.logo.svg', [
+                'alt' => __('Minderest'), 
+                'class' => 'logo img-fluid',
+            ]),
+            '/', 
+            ['escape' => false, 'class'=> 'navbar-brand col-md-3 col-lg-2 mr-0 px-3']
+        )?>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+        <input class="form-control form-control-dark w-100" type="text" placeholder="Buscar..." aria-label="Search">
     </nav>
     <!--- Container -->
     <div class="container-fluid">
